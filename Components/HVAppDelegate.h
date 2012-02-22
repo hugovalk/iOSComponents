3eea3219f8c5d293eb5f1188385be92511fd6a91
Hugo Valk <hugovalk@Hugo-Valks-MacBook-Pro.local>
2012-02-22 21:13:36 +0100

    Initial Commit

---
+++ Components/HVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HVAppDelegate.h
+//  Components
+//
+//  Created by Valk Hugo on 2/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Components/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Components
+//
+//  Created by Valk Hugo on 2/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HVAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HVAppDelegate class]));
+    }
+}
