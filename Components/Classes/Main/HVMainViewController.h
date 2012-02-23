427be6463b7c4e04882c48a3a88c614756b62fa7
Hugo Valk <hugo.valk@gmail.com>
2012-02-23 07:52:31 +0100

    Set up a main UI to navigate components

---
+++ Components/Classes/Main/HVMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  HVMainViewController.h
+//  Components
+//
+//  Created by Valk Hugo on 2/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HVMainViewController : UITableViewController
+
+@end
